4f9d3cc6354dddcaaa7326ac80fcf0ad207f4eba
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2019-01-25 16:22:02 +0530

    Fixed the HomeController
    Wrote the login in HomeController to check if user is authenticated and redirect accordingly.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,11 @@
 
 namespace Atomix\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
@@ -23,6 +14,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::check()) {
+            return view('home');
+        }
+        else {
+            return view('welcome');
+        }
     }
 }
