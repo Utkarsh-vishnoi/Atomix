bf182350e197ee67c10383e70f8308db6db0e92a
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2019-01-31 15:14:08 +0530

    Created Atom table Migration

---
+++ database/migrations/2019_01_31_093123_create_atoms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atoms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->text('description');
+            $table->text('skel');
+            $table->text('style');
+            $table->text('func');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atoms');
+    }
+}
